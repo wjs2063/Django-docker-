51ec014d736abe4f0943426ab049e584f4fadc4b
Jae hyeon <wjs2063@naver.com>
2022-06-04 14:54:35 +0000

    lottocode

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class lotto(models.Model):
+    name=models.CharField(max_length=10)
+    
+    number=models.IntegerField(" 금액을 입력하세요 ",default=1000,help_text="천원에서 20만원까지 입력하세요")
+    create_date=models.DateTimeField()
+
